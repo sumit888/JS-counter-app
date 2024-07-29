22c268392dcbc70471abe6ac0dcb205ff5359a9a
np03s190038 <thapa.sumit007@gmail.com>
2024-07-29 17:36:43 +0545

    new stuff

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 // document.getElementById("count").innerText = 2;
 
+let countEl = document.getElementById("count-el") //pass in arguments to function
+
+// console.log(countEl)
+
 let count = 0;
-console.log(count)
+
+function increment(){
+ count = count+1; 
+ countEl.innerText = count
+//  console.log(count)
+}
+
+
+function save(){
+    console.log(count)
+}
